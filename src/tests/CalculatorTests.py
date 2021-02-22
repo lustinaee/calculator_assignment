788aa741ffac37495911115b0ceac7fadbd0a9ce
lustinaee <71726608+lustinaee@users.noreply.github.com>
2021-02-22 18:15:55 -0500

    Featured: setUp method

--- src/tests/CalculatorTests.py
+++ src/tests/CalculatorTests.py
@@ -5,7 +5,9 @@ from CsvReader import CsvReader
 
 
 class MyTestCase(unittest.TestCase):
-
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+        
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
