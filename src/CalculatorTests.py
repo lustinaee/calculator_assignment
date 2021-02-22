e9854a02cb5f7769fef3ea3bfe44dfa755d15fbf
Austin <peaustinlee@gmail.com>
2021-02-22 02:13:34 -0500

    Task: added addition method

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,5 +1,5 @@
 import unittest
-from Calculator import Calculator
+from operations.Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
@@ -10,11 +10,38 @@ class MyTestCase(unittest.TestCase):
 
     def test_results_property_calculator(self):
         calculator = Calculator()
-        self.assertEqual(calculator.result, 4)
+        self.assertEqual(calculator.result, 0)
 
     def test_add_method_calculator(self):
         calculator = Calculator()
         self.assertEqual(calculator.add(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
+
+    def test_subtract_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.subtract(2, 2), 0)
+        self.assertEqual(calculator.result, 0)
+
+    def test_multiply_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.multiply(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
+
+    def test_divide_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.divide(2, 2), 1)
+        self.assertEqual(calculator.result, 1)
+
+    def test_square_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.square(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
+
+    def test_root_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.root(4), 2)
+        self.assertEqual(calculator.result, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
---
+++ src/operations/Calculator.py
@@ -0,0 +1,35 @@
+
+
+def subtraction(a, b):
+    return a - b
+
+
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+    def add(self, a, b):
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+    def square(self, a):
+        self.result = squared(a, b)
+        return self.result
+
+    def root(self, a, b):
+        self.result = rooted(a)
+        return self.result
