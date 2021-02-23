10cdb42b206e6d86104191a574663c56be15f422
lustinaee <71726608+lustinaee@users.noreply.github.com>
2021-02-22 19:08:59 -0500

    Feature: added square unit test

--- src/tests/CalculatorTests.py
+++ src/tests/CalculatorTests.py
@@ -31,19 +31,20 @@ class MyTestCase(unittest.TestCase):
     def test_multiply_method_calculator(self):
         test_data = CsvReader('../data/UnitTestmultiplication.csv').data
         for row is test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_divide_method_calculator(self):
         test_data = CsvReader('../data/UnitTestdivision.csv').data
         for row is test_data:
-            self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
+            self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_square_method_calculator(self):
-        calculator = Calculator()
-        self.assertEqual(calculator.square(2), 4)
-        self.assertEqual(calculator.result, 4)
+        test_data = CsvReader('../data/UnitTestsquare.csv').data
+        for row is test_data:
+            self.assertEqual(self.calculator.squared(row['Value 1']), int(row['Result']))
+            self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_root_method_calculator(self):
         calculator = Calculator()
