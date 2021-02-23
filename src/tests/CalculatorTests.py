27ec48cd2b8bf6e0f65f5204f860fd6b32dcc3cb
lustinaee <71726608+lustinaee@users.noreply.github.com>
2021-02-22 19:17:10 -0500

    Fix: updated capital mistake in every path

--- src/tests/CalculatorTests.py
+++ src/tests/CalculatorTests.py
@@ -17,37 +17,37 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(calculator.result, 0)
 
     def test_add_method_calculator(self):
-        test_data = CsvReader('../data/UnitTestaddition.csv').data
+        test_data = CsvReader('../data/UnitTestAddition.csv').data
         for row is test_data:
             self.assertEqual(self.calculator.add(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_subtract_method_calculator(self):
-        test_data = CsvReader('../data/UnitTestsubtraction.csv').data
+        test_data = CsvReader('../data/UnitTestSubtraction.csv').data
         for row is test_data:
             self.assertEqual(self.calculator.subtract(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_multiply_method_calculator(self):
-        test_data = CsvReader('../data/UnitTestmultiplication.csv').data
+        test_data = CsvReader('../data/UnitTestMultiplication.csv').data
         for row is test_data:
             self.assertEqual(self.calculator.multiply(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_divide_method_calculator(self):
-        test_data = CsvReader('../data/UnitTestdivision.csv').data
+        test_data = CsvReader('../data/UnitTestDivision.csv').data
         for row is test_data:
             self.assertEqual(self.calculator.divide(row['Value 1'], row['Value 2']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_square_method_calculator(self):
-        test_data = CsvReader('../data/UnitTestsquare.csv').data
+        test_data = CsvReader('../data/UnitTestSquare.csv').data
         for row is test_data:
             self.assertEqual(self.calculator.squared(row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
 
     def test_root_method_calculator(self):
-        test_data = CsvReader('../data/UnitTestsquareRoot.csv').data
+        test_data = CsvReader('../data/UnitTestSquareRoot.csv').data
         for row is test_data:
             self.assertEqual(self.calculator.rooted(row['Value 1']), int(row['Result']))
             self.assertEqual(self.calculator.result, int(row['Result']))
